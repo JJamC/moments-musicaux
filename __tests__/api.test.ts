d683628e7a1b54c0e7b91438d96ab7aa2c5bb692
JJamC <jaycarroll97@live.co.uk>
2025-05-16 15:44:25 +0100

    Revert "added tests"
    This reverts commit 66f54d83df6d5fbed62c2c975e4715f420610fce.

--- __tests__/api.test.ts
+++ __tests__/api.test.ts
@@ -3,7 +3,6 @@ import { afterEach, beforeEach } from "node:test";
 import { data } from "../prisma/data/index";
 import { seed } from "../prisma/seed";
 import { findTopics } from "~/server/api/routers/models/topics.model";
-import { fetchUser, findUsers } from "~/server/api/routers/models/users.model";
 
 beforeEach(async () => {
   console.log(process.env.NODE_ENV);
@@ -16,40 +15,7 @@ afterEach(async () => {
   await prisma.$disconnect();
 });
 
-it("findTopics should return an array of topics with slug and description properties", async () => {
+it("should return a table of topics with slug and description properties", async () => {
   const topics = await findTopics();
-  expect(topics.length).toBe(7);
-  topics.forEach((topic) => {
-    expect(topic).toMatchObject({
-      description: expect.any(String),
-      slug: expect.any(String),
-    });
-  });
+  console.log(topics);
 });
-
-it("findUsers should return an array of users", async () => {
-  const users = await findUsers()
-  expect(users.length).toBe(4)
-  users.forEach((user) => {
-    expect(user).toMatchObject({
-      id: expect.any(Number),
-      createdAt: expect.any(String),
-      username: expect.any(String),
-      email: expect.any(String),
-      avatar_url: expect.any(String)
-    })
-  })
-})
-
-it("fetchUser should return a specific user", async () => {
-  const user = await fetchUser(2);
-    expect(user).toMatchObject({
-      username: "ravelM",
-      email: "ravelM@impressionistmusic.com",
-      avatar_url:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Maurice_Ravel_1925.jpg/1024px-Maurice_Ravel_1925.jpg",
-      createdAt: "2023-05-16T08:45:00Z",
-    });
-});
-
-
